49b6fd460ac43cf40394f44559b9feb45bd46ba1
GiorgosSimos <giorgos_Simos@windowslive.com>
2024-10-01 19:45:03 +0300

    - Implementation of RecyclerView to display search results

--- app/src/main/java/com/unipi/mobile_dev/hippocratesjournal/SearchActivity.java
+++ app/src/main/java/com/unipi/mobile_dev/hippocratesjournal/SearchActivity.java
@@ -11,16 +11,8 @@ import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.Spinner;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
-
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.Query;
-import com.google.firebase.database.ValueEventListener;
 
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
@@ -33,15 +25,11 @@ public class SearchActivity extends AppCompatActivity {
     Spinner searchCriteriaSpinner;
     private EditText searchNameEditText, searchDoeEditText, searchDiagnosisEditText;
     private Button searchButton;
-    private FirebaseDatabase database;
-    private DatabaseReference reference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
-        database = FirebaseDatabase.getInstance();
-        reference = database.getReference("incidents");
         searchNameEditText = findViewById(R.id.searchNameEditText);
         searchNameEditText.setVisibility(View.INVISIBLE);
         searchDoeEditText = findViewById(R.id.searchDoeEditText);
