8850676ec26694b788e67c11a4e02b4970603986
GiorgosSimos <giorgos_Simos@windowslive.com>
2024-10-03 00:13:05 +0300

    - Implemented a back button on New Incident Activity and Search Incident Activity

--- app/src/main/java/com/unipi/mobile_dev/hippocratesjournal/NewIncidentActivity.java
+++ app/src/main/java/com/unipi/mobile_dev/hippocratesjournal/NewIncidentActivity.java
@@ -1,19 +1,15 @@
 package com.unipi.mobile_dev.hippocratesjournal;
 
-import android.annotation.SuppressLint;
 import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
-import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.Spinner;
 
-import androidx.activity.OnBackPressedCallback;
-import androidx.activity.OnBackPressedDispatcher;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import com.google.firebase.database.DatabaseReference;
