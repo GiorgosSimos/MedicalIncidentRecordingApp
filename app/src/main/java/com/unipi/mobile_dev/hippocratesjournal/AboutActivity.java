7dbbb4c41e4d8aedd98357574ef65c8c777b7fec
GiorgosSimos <giorgos_Simos@windowslive.com>
2024-10-03 14:31:23 +0300

    - Implemented navigation drawer in Main Screen

---
+++ app/src/main/java/com/unipi/mobile_dev/hippocratesjournal/AboutActivity.java
@@ -0,0 +1,14 @@
+package com.unipi.mobile_dev.hippocratesjournal;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class AboutActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+    }
+}
--- app/src/main/java/com/unipi/mobile_dev/hippocratesjournal/MainScreenActivity.java
+++ app/src/main/java/com/unipi/mobile_dev/hippocratesjournal/MainScreenActivity.java
@@ -1,24 +1,33 @@
 package com.unipi.mobile_dev.hippocratesjournal;
 
+import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.text.Html;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import java.util.Locale;
 
 public class MainScreenActivity extends AppCompatActivity {
 
+    DrawerLayout drawerLayout;
+    ImageView menu_icon;
+    LinearLayout home, all_incidents, about, contact, login_logout;
     SharedPreferences sharedPreferences;
     Locale locale = Locale.getDefault();
     String language = locale.getLanguage();
-    TextView bulletTextView;
+    TextView bulletTextView, user_info, login_logout_text;
     String userType, bulletText = "";
 
     @Override
@@ -28,6 +37,45 @@ public class MainScreenActivity extends AppCompatActivity {
         sharedPreferences = getApplicationContext().getSharedPreferences("com.unipi.mobile_dev.hippocratesjournal",
                 Context.MODE_PRIVATE);
         userType = sharedPreferences.getString("UserType", "");
+        drawerLayout = findViewById(R.id.drawerLayout);
+        menu_icon = findViewById(R.id.burger_menu);
+        user_info = findViewById(R.id.user_info);
+        // Link options of hamburger menu
+        home = findViewById(R.id.home);
+        all_incidents = findViewById(R.id.all_incidents);
+        about = findViewById(R.id.about);
+        contact = findViewById(R.id.contact);
+        login_logout = findViewById(R.id.login_logout);
+        login_logout_text = findViewById(R.id.login_logout_text);
+        if (userType.equals("Visitor")) {
+            login_logout_text.setText(getString(R.string.login_logout));
+        } else {
+            login_logout_text.setText(getString(R.string.logout));
+        }
+        user_info.setText(userType);
+        menu_icon.setOnClickListener(v -> openDrawer(drawerLayout));
+        home.setOnClickListener(v -> {recreate();});
+        all_incidents.setOnClickListener(v -> {redirectActivity(MainScreenActivity.this,
+                DisplayAllIncidentsActivity.class);});
+        about.setOnClickListener(v -> {redirectActivity(MainScreenActivity.this, AboutActivity.class);});
+        contact.setOnClickListener(v -> {redirectActivity(MainScreenActivity.this, ContactActivity.class);});
+        login_logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (userType.equals("Visitor")) {// User is signed in as guest
+                        Toast.makeText(MainScreenActivity.this,
+                                getString(R.string.login_singup), Toast.LENGTH_SHORT).show();
+                } else {// User has signed in with credentials
+                        Toast.makeText(MainScreenActivity.this,
+                                getString(R.string.logout_successful), Toast.LENGTH_SHORT).show();
+                }
+                redirectActivity(MainScreenActivity.this, WelcomeActivity.class);
+                SharedPreferences.Editor prefsEditor = sharedPreferences.edit();
+                prefsEditor.clear();
+                prefsEditor.apply();
+            }
+
+        });
         bulletTextView = findViewById(R.id.bulletTextView);
         if (language.equals("es")) {
             bulletText = "<p>La aplicación te permite realizar las siguientes tareas:</p>" +
@@ -46,15 +94,35 @@ public class MainScreenActivity extends AppCompatActivity {
                     "<li>Display All incidents </li>" +
                     "</ul>";
         }
-
         bulletTextView.setText(Html.fromHtml(bulletText, Html.FROM_HTML_MODE_COMPACT));
+    }
+
+    public static void openDrawer(DrawerLayout drawerLayout){
+        drawerLayout.openDrawer(GravityCompat.START);
+    }
+
+    public static void closeDrawer(DrawerLayout drawerLayout){
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)){
+            drawerLayout.closeDrawer(GravityCompat.START);
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        closeDrawer(drawerLayout);
+    }
+
+    public static void redirectActivity(Activity activity, Class secondActivity){
+        Intent intent = new Intent(activity, secondActivity);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        activity.startActivity(intent);
     }
 
     public void goNewIncident(View view) {
         if (!userType.equals("Visitor")) {
             Intent intent = new Intent(MainScreenActivity.this, NewIncidentActivity.class);
             startActivity(intent);
-            finish();
         } else {
             showAlert(getString(R.string.new_incident), getString(R.string.user_restriction));
         }
